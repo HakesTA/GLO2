341b1e6359c27f162a4169f6679bf9f0858db3e7
Thomas Hakes <thakes3@lab46.corning-cc.edu>
2014-11-04 21:14:39 -0500

    working javascript functional design perhaps

---
+++ header.js
@@ -0,0 +1,7 @@
+var slideimages = new Array() // create new array to preload images
+slideimages[0] = new Image() // create new instance of image object
+slideimages[0].src = "header1.jpg" // set image object src property to an image's src, preloading that image in the process
+slideimages[1] = new Image()
+slideimages[1].src = "header2.jpg"
+slideimages[2] = new Image()
+slideimages[2].src = "header3.png"
